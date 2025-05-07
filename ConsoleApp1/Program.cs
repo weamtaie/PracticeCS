d99e2377c9529864f35efcc7770430bb35f56c42
weamtaie <Weamtaie2002@gmail.com>
2025-05-07 10:28:26 +0300

    Example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,214 +5,69 @@ using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
 
-Console.WriteLine("-----------");
-Console.WriteLine();
-
-Dog Mydog=new Dog();
-Mydog.MakeSound();
-
-Console.WriteLine();
-Console.WriteLine("-----------");
-Console.WriteLine();
-
-DataPrint dataPrint = new DataPrint();
-dataPrint.print(2);
-dataPrint.print("hi 2");
-dataPrint.print(2.2);
 
 
-Console.WriteLine();
-Console.WriteLine("-----------");
-Console.WriteLine();
+Box box1=new Box();
+Box box2 = new Box();
+Box box3 = new Box();
 
-Point a = new Point(1,2);
-Point b = new Point(2,3);
+ double volume;
 
-Point c = a + b;
-Console.WriteLine($"the result is :{c}");
+box1.SetLength(2);
+box1.SetWidth(2);
+box1.SetBredth(2);
 
-Console.WriteLine();
-Console.WriteLine("-----------");
-Console.WriteLine();
-
-Rectangle r = new Rectangle(10, 7);
-int RecArea=r.area();
-Console.WriteLine($"Rectangle class area :{ RecArea}");
-
-Triangle t = new Triangle(10, 5);
-int TraiArea = t.area();
-Console.WriteLine($"traingle class area :{TraiArea}");
-
-Shape sh = new Shape();
-
-Caller c1 =new Caller();
-Caller.CallArea(sh);
-
-Caller.CallArea(t);
-Caller.CallArea(r);
-c1.Print();
+volume=box1.GetVolume();
+Console.WriteLine($"box 1 volume : {volume}");
 
 
-Console.WriteLine();
-Console.WriteLine("-----------");
-Console.WriteLine();
+box2.SetLength(3);
+box2.SetWidth(3);
+box2.SetBredth(3);
 
-IAnimal animal;
-animal=new Lion();
-animal.MakeSound();
-
-
-animal = new Cat();
-animal.MakeSound();
-//class
-
-
-//abstract class Animal
-//{
-//    public abstract void MakeSound();  // No implementation
-//}
-
-//class Dog : Animal
-//{
-//    public override void MakeSound()
-//    {
-//        Console.WriteLine("Dog barks.");
-//    }
-//}
+volume = box2.GetVolume();
+Console.WriteLine($"box 2 volume : {volume}");
 
 
 
-
-//Runtime polymorphism (Method Overriding)
-class Animal
+box3 = box1 + box2;
+volume = box3.GetVolume();
+Console.WriteLine($"volume of box3 is : {volume}");
+public class Box
 {
-    public virtual void MakeSound()
-    {
-        Console.WriteLine("Animal speaks");
-    }
-}
+    private double length;
+    private double width;
+    private double bredth;
 
-class Dog : Animal
-{
-    public override void MakeSound()
-    {
-        Console.WriteLine("Dog barks");
-    }
-}
-
-//Function Overloading
-class DataPrint
-{
-    public void print(int i)
-    {
-        Console.WriteLine(i);    
-    }
-    public void print(double f)
-    {
-        Console.WriteLine(f);
-    }
-    public void print(string s)
-    {
-        Console.WriteLine(s);
-    }
-}
-
-class Point
-{
-    private int X;
-    private int Y; 
-
-    public Point (int x, int y)
-    {
-        X = x;  
-        Y = y;
-    }
-
-    public static Point operator +(Point p1, Point p2)
-    {
-        return new Point((p1.X+ p1.Y),(p2.X+p2.Y));
-    }
-    //The ToString() method is
-    //a function (or method)
-    //designed to return a string
-    //representation of an object.
-    public override string ToString()
-    {
-        return $"({X}, {Y})";
-    }
-}
-
-
-////////////////////////////////////////////////
-public class Shape {
-protected int width, height;
-
-public Shape(int a = 0, int b = 0)
-{
-    width = a;
-    height = b;
-}
-public virtual int area()
-{
-    Console.WriteLine("Parent class area :");
-    return 0;
-}
-   }
-   class Rectangle : Shape
-{
-    public Rectangle(int a = 0, int b = 0) : base(a, b)
-    {
-
-    }
-    public override int area()
-    {
-        
-        return (width * height);
-    }
-}
-class Triangle : Shape
-{
-    public Triangle(int a = 0, int b = 0) : base(a, b)
-    {
-    }
-    public override int area()
-    {
-        
-        return (width * height / 2);
-    }
-}
-public class Caller
-{
- 
-    public void Print()
-    {
-        Console.WriteLine("vvv");
-    }
-    public static void CallArea(Shape sh)
-    {
-        int a;
-        a = sh.area();
-        Console.WriteLine("Area: {0}", a);
-    }
-}
-
-
-public interface IAnimal
-{
-    void MakeSound();
     
-}
-public class Lion:IAnimal
-{
-    public void MakeSound()
+
+    public void SetLength(double len)
     {
-        Console.WriteLine("lion make sound");
+        length = len;
     }
-}
-public class Cat : IAnimal
-{
-    public void MakeSound()
+    public void SetWidth(double wid)
     {
-        Console.WriteLine("Cat make sound");
+        width = wid;
     }
+    public void SetBredth(double bred)
+    {
+        bredth =bred ;
+    }
+
+    public double GetVolume()
+    {
+        return length*width*bredth;
+    }
+
+    public static Box operator + (Box a, Box b)
+    {
+        Box box=new Box();
+        box.length = a.length+b.length;
+        box.width = a.width+b.width;
+        box.bredth= a.bredth+b.bredth;
+
+        return box;
+
+    }
+
 }
